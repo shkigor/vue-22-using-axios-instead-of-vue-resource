f343b32065560e64f4516752a7b6d3cf57148230
Igor Shklyar <igor.shklyar@gmail.com>
2018-02-16 12:47:01 +0200

    338. Using Interceptors How to remove Interceptors

--- src/main.js
+++ src/main.js
@@ -9,15 +9,18 @@ axios.defaults.baseURL = 'https://vuejs-http-23956.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'something';
 axios.defaults.headers.get['Accepts'] = 'application/json';
 
-axios.interceptors.request.use(config => {
+const reqInterceptor = axios.interceptors.request.use(config => {
     console.log('Request Interceptor', config);
     config.headers['SMTH_IN_INTERCEPTORS'] = 'foo';
     return config;
 });
-axios.interceptors.response.use(res => {
+const resInterceptor = axios.interceptors.response.use(res => {
     console.log('Response Interceptor', res);
     return res;
 });
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
 
 new Vue({
   el: '#app',

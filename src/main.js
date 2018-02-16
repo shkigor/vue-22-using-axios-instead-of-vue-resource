1c38de3dfe0795736bfcbf5274a29a12dc3984fb
Igor Shklyar <igor.shklyar@gmail.com>
2018-02-16 13:38:12 +0200

    Added https://github.com/rstacruz/nprogress

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import axios from 'axios'
 import router from './router'
 import store from './store'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
 axios.defaults.baseURL = 'https://vuejs-http-23956.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'something';
 axios.defaults.headers.get['Accepts'] = 'application/json';
@@ -12,15 +16,17 @@ axios.defaults.headers.get['Accepts'] = 'application/json';
 const reqInterceptor = axios.interceptors.request.use(config => {
     console.log('Request Interceptor', config);
     config.headers['SMTH_IN_INTERCEPTORS'] = 'foo';
+    NProgress.start();
     return config;
 });
 const resInterceptor = axios.interceptors.response.use(res => {
     console.log('Response Interceptor', res);
+    NProgress.done();
     return res;
 });
 
-axios.interceptors.request.eject(reqInterceptor);
-axios.interceptors.response.eject(resInterceptor);
+// axios.interceptors.request.eject(reqInterceptor);
+// axios.interceptors.response.eject(resInterceptor);
 
 new Vue({
   el: '#app',
